17f1ec26e50b72eed04e8a3cb4d8f28f8d889b90
Collins Odue <collinsod66@gmail.com>
2024-06-23 23:19:59 +0000

    Trying out python first time

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 5
+
+result1 = number1 + number2
+result2 = number1 * number2
+result3 = number1 - number2
+
+print ( "Addition of 10 and 5 is", result1 )
+print ( "Multiplication of 10 and 5 is", result2 )
+print ( "Subtraction of 10 and 5 is ", result3 )
+
+
+
